eceb259d4641c831b635fa91a79a3b6e013473c8
harikapallapothu <harika3412@gmail.com>
2021-09-04 14:22:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/script.js">
+    <link rel ="stylesheet" href="/script.css.css"
+    <script src="/script.js"></script> 
+  </head>
+  <body>
+    <div class = "card">
+        <img class="cardPhoto" src="https://cdn.britannica.com/97/1597-004-05816F4E/Flag-India.jpg">
+      <div class="cardData">
+        <h1>India</h1>
+        <p><b>Population: </b> 1,395,864,796</p>
+        <p><b> Region:</b> Asia</p>
+        <p><b> Capital:</b> New Delhi</p>
+      </div>
+    </div>
+    <div class = "card">
+      <img class="cardPhoto" src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/383px-Flag_of_Germany.svg.png">
+    <div class="cardData">
+      <h1>Germany</h1>
+      <p><b>Population: </b> 81,770,900</p>
+      <p><b> Region:</b> Europe</p>
+      <p><b> Capital:</b> Berlin</p>
+    </div>
+  </div>
+  <div class = "card">
+    <img class="cardPhoto" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png">
+  <div class="cardData">
+    <h1>USA</h1>
+    <p><b>Population: </b> 323,924,000</p>
+    <p><b> Region:</b> Americas</p>
+    <p><b> Capital:</b> Washington. D.C</p>
+  </div>
+</div>
+<div class = "card">
+  <img class="cardPhoto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/2560px-Flag_of_Iceland.svg.png">
+<div class="cardData">
+  <h1>Iceland</h1>
+  <p><b>Population: </b>334,300</p>
+  <p><b> Region:</b> Europe</p>
+  <p><b> Capital:</b> Reykjavik</p>
+</div>
+</div>
+
+
+  </body>
+</html>
